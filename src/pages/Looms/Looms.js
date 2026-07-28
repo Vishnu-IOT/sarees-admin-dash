29a450d26874a89d86fe03158185f515a3397098
Vishnu-IOT <vishnubharani153@gmail.om>
2026-07-28 15:32:50 +0530

    updates udpates

--- src/pages/Looms/Looms.js
+++ src/pages/Looms/Looms.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { useData } from "../../context/DataContext";
 import AddToLoomModal from "./AddToLoomModal";
 import "./Looms.css";
